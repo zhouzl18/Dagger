0cf2bba23f451c563bcb63d3ecc8c88bb3eab5a3
zhouzl18 <fenglianhai18@gmail.com>
2017-11-28 00:38:10 +0800

    Create a SharedPrefsHelper to deal with the shared preferences.

---
+++ app/src/main/java/com/oneday/daggerdemo/data/SharedPrefsHelper.java
@@ -0,0 +1,55 @@
+package com.oneday.daggerdemo.data;
+
+import android.content.SharedPreferences;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Created by fengl on 2017-11-28.
+ */
+
+@Singleton
+public class SharedPrefsHelper {
+
+    public static final String PREF_KEY_ACCESS_TOKEN = "access-token";
+
+    private SharedPreferences mSharedPreferences;
+
+    @Inject
+    public SharedPrefsHelper(SharedPreferences mSharedPreferences) {
+        this.mSharedPreferences = mSharedPreferences;
+    }
+
+    public void put(String key, String value){
+        mSharedPreferences.edit().putString(key, value).apply();
+    }
+
+    public void put(String key, int value){
+        mSharedPreferences.edit().putInt(key, value).apply();
+    }
+
+    public void put(String key, float value){
+        mSharedPreferences.edit().putFloat(key, value).apply();
+    }
+
+    public void put(String key, boolean value){
+        mSharedPreferences.edit().putBoolean(key, value).apply();
+    }
+
+    public String get(String key, String defaultValue){
+        return mSharedPreferences.getString(key, defaultValue);
+    }
+
+    public Integer get(String key, int defaultValue){
+        return mSharedPreferences.getInt(key, defaultValue);
+    }
+
+    public Float get(String key, float defaultValue){
+        return mSharedPreferences.getFloat(key, defaultValue);
+    }
+
+    public Boolean get(String key, boolean defaultValue){
+        return mSharedPreferences.getBoolean(key, defaultValue);
+    }
+}
